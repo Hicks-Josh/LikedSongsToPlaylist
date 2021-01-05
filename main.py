e43d137729280e13ab904ba6ce8ec2154a107f30
jhicks18 <jhicks18@georgefox.edu>
2021-01-04 21:19:38 -0800

    Initial Commit
    Added script along with a credentials file

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
+from json.decoder import JSONDecodeError
+import spotify_credentials as sc
+
+scope = 'user-read-private playlist-modify-public user-library-read'
+username = sc.username
+client_id = sc.client_id
+client_secret = sc.client_secret
+redirect_uri = 'https://www.google.com/callback/'
+playlist_id = sc.playlist_id
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+
+# Erase cache and prompt for user permission
+try:
+    # token = client_credentials_manager.get_access_token()
+    token = util.prompt_for_user_token(username=username, scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)  # add scope
+except (AttributeError, JSONDecodeError):
+    os.remove(f".cache-{username}")
+    token = util.prompt_for_user_token(username=username, scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)  # add scope
+    # token = client_credentials_manager.get_access_token()
+
+# Create spotify object with permissions
+spotifyObject = spotipy.Spotify(auth=token)
+
+# due to how results gets put into paginated sections,
+# and due to the issue where there is a limit to how many
+# songs can be moved at a time, every time I get a section
+# I immediately upload them
+#
+# it seems like there is a cap to how many can still be uploaded at a time
+# so whenever it stops uploading I just take the limit and add it to the offset
+results = spotifyObject.current_user_saved_tracks(offset=0)
+tracks = results['items']
+while results['next']:
+    track_uris = []
+    for track in tracks:
+        track_uris.append(track['track']['uri'])
+        print(track['track']['name'])
+    spotifyObject.user_playlist_add_tracks(username, playlist_id,
+                                           track_uris)
+    
+    results = spotifyObject.next(results)
+    tracks = results['items']
